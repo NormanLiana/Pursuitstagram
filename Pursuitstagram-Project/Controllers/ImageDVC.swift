edbe908c30292cf9ae3478fe87f06fe8ff30ef40
NormanLiana <liananorman@pursuit.org>
2019-11-22 22:16:23 -0500

    adds Image DVC file in controllers folder

---
+++ Pursuitstagram-Project/Controllers/ImageDVC.swift
@@ -0,0 +1,30 @@
+//
+//  ImageDVC.swift
+//  Pursuitstagram-Project
+//
+//  Created by Liana Norman on 11/22/19.
+//  Copyright © 2019 Liana Norman. All rights reserved.
+//
+
+import UIKit
+
+class ImageDVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
