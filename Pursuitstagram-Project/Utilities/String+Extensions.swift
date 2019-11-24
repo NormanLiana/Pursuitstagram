edd03dd86e751bd8a71e407548c4c6a08c403729
NormanLiana <liananorman@pursuit.org>
2019-11-24 17:18:12 -0500

    adds file for strings and extensions in utilities folder

---
+++ Pursuitstagram-Project/Utilities/String+Extensions.swift
@@ -0,0 +1,9 @@
+//
+//  String+Extensions.swift
+//  Pursuitstagram-Project
+//
+//  Created by Liana Norman on 11/24/19.
+//  Copyright © 2019 Liana Norman. All rights reserved.
+//
+
+import Foundation
