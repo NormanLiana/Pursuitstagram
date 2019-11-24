395d949541e133cae7e308cf6d703962c7b59b38
NormanLiana <liananorman@pursuit.org>
2019-11-24 12:34:44 -0500

    adds folder for Views and adds CV Cell cocoa file for feed to that folder

---
+++ Pursuitstagram-Project/Views/FeedCVCell.swift
@@ -0,0 +1,13 @@
+//
+//  FeedCVCell.swift
+//  Pursuitstagram-Project
+//
+//  Created by Liana Norman on 11/24/19.
+//  Copyright © 2019 Liana Norman. All rights reserved.
+//
+
+import UIKit
+
+class FeedCVCell: UICollectionViewCell {
+    
+}
