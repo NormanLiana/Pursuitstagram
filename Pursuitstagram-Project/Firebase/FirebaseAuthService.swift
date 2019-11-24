1ad65091ebb91d9aec09307b6bce1ce42927776c
NormanLiana <liananorman@pursuit.org>
2019-11-24 17:14:00 -0500

    builds methods to create and log in a user

--- Pursuitstagram-Project/Firebase/FirebaseAuthService.swift
+++ Pursuitstagram-Project/Firebase/FirebaseAuthService.swift
@@ -7,3 +7,37 @@
 //
 
 import Foundation
+import FirebaseAuth
+
+class FirebaseAuthService {
+    
+    static let manager = FirebaseAuthService()
+    
+    private let auth = Auth.auth()
+    
+    var currentUser: User? {
+        return auth.currentUser
+    }
+    
+    func createNewUser(email: String, password: String, completion: @escaping (Result<User, Error>) -> () ) {
+        auth.createUser(withEmail: email, password: password) { (result, error) in
+            if let createdUser = result?.user {
+                completion(.success(createdUser))
+            } else if let error = error {
+                completion(.failure(error))
+            }
+        }
+    }
+    
+    func logInUser(email: String, password: String, completion: @escaping (Result<(), Error>) -> () ) {
+        auth.signIn(withEmail: email, password: password) { (result, error) in
+            if let _ = result?.user {
+                completion(.success(()))
+            } else if let error = error {
+                completion(.failure(error))
+            }
+        }
+    }
+    
+    private init () {}
+}
