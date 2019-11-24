0698114305b6905d27e38e54e3262132aaae6128
NormanLiana <liananorman@pursuit.org>
2019-11-24 12:35:59 -0500

    adds all Ui objects for cell and sets constraints in content view

--- Pursuitstagram-Project/Views/FeedCVCell.swift
+++ Pursuitstagram-Project/Views/FeedCVCell.swift
@@ -10,4 +10,46 @@ import UIKit
 
 class FeedCVCell: UICollectionViewCell {
     
+    // MARK: - UI Objects
+    lazy var imageInFeed: UIImageView = {
+       let img = UIImageView()
+        img.backgroundColor = .darkGray
+        return img
+    }()
+    
+    lazy var postedByLabel: UILabel = {
+       let label = UILabel()
+        label.text = "Submitted By:"
+        label.font = UIFont(name: "Futura-CondensedExtraBold", size: 18)
+        label.backgroundColor = .clear
+        label.textAlignment = .center
+        label.textColor = .systemPink
+        return label
+    }()
+    
+    // MARK: - Initializers
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        contentView.addSubview(imageInFeed)
+        contentView.addSubview(postedByLabel)
+        constrainImage()
+        constrainLabel()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Contraint Methods
+    private func constrainImage() {
+        imageInFeed.translatesAutoresizingMaskIntoConstraints = false
+        
+        [imageInFeed.topAnchor.constraint(equalTo: contentView.topAnchor), imageInFeed.leadingAnchor.constraint(equalTo: contentView.leadingAnchor), imageInFeed.trailingAnchor.constraint(equalTo: contentView.trailingAnchor), imageInFeed.bottomAnchor.constraint(equalTo: contentView.bottomAnchor)].forEach{$0.isActive = true}
+    }
+    
+    private func constrainLabel() {
+        postedByLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        [postedByLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor), postedByLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor), postedByLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor), postedByLabel.heightAnchor.constraint(equalTo: contentView.heightAnchor, multiplier: 0.2)].forEach({$0.isActive = true})
+    }
 }
