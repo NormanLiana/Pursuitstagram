5121088063ac272ccbf4959522be0ad77e5aa7b9
NormanLiana <liananorman@pursuit.org>
2019-11-24 17:58:20 -0500

    builds app user model to work with firebase auth

--- Pursuitstagram-Project/Models/AppUser.swift
+++ Pursuitstagram-Project/Models/AppUser.swift
@@ -7,3 +7,37 @@
 //
 
 import Foundation
+import FirebaseAuth
+import FirebaseFirestore
+
+struct AppUser {
+    let email: String?
+    let uid: String
+    let userName: String?
+    let photoURL: String?
+    
+    init(from user: User) {
+        self.email = user.email
+        self.uid = user.uid
+        self.userName = user.displayName
+        self.photoURL = user.photoURL?.absoluteString
+    }
+    
+    init?(from dict: [String:Any], id: String) {
+        guard let email = dict["email"] as? String,
+        let userName = dict["userName"] as? String,
+        let photoURL = dict["photoURL"] as? String else {return nil}
+        
+        self.email = email
+        self.uid = id
+        self.userName = userName
+        self.photoURL = photoURL
+    }
+    
+    var fieldsDict: [String:Any] {
+        return [
+            "userName": self.userName ?? "",
+            "email": self.email ?? ""
+        ]
+    }
+}
