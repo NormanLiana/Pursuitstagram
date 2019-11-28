6dd8ed1a9c7ee523a1977b368b90bee641d25c60
NormanLiana <liananorman@pursuit.org>
2019-11-27 19:43:23 -0500

    builds model and it's properties for a Post

--- Pursuitstagram-Project/Models/Post.swift
+++ Pursuitstagram-Project/Models/Post.swift
@@ -10,5 +10,31 @@ import Foundation
 import FirebaseFirestore
 
 struct Post {
+    let photoURL: String
+    let id: String
+    let dateCreated: Date?
+    let creatorID: String
     
+    init(photoURL: String, creatorID: String, dateCreated: Date? = nil) {
+        self.photoURL = photoURL
+        self.id = UUID().description
+        self.creatorID = creatorID
+        self.dateCreated = dateCreated
+    }
+    
+    init?(from dict: [String : Any], id: String) {
+        guard let photoURL = dict["photoURL"] as? String,
+        let creatorID = dict["creatorID"] as? String,
+        let dateCreated = (dict["dateCreated"] as? Timestamp)?.dateValue() else {return nil}
+        
+        self.photoURL = photoURL
+        self.creatorID = creatorID
+        self.dateCreated = dateCreated
+        self.id = id
+    }
+    
+    var fieldsDict: [String : Any] {
+        return ["photoURL" : self.photoURL, "creatorID" : self.creatorID]
+    }
 }
+
