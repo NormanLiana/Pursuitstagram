99b6bd17c87db65d8df7819358711cf9702d5681
NormanLiana <liananorman@pursuit.org>
2019-11-27 19:45:23 -0500

    builds firebase storage service to create and update a user as well as create and get posts

--- Pursuitstagram-Project/Firebase/FirestoreService.swift
+++ Pursuitstagram-Project/Firebase/FirestoreService.swift
@@ -7,3 +7,117 @@
 //
 
 import Foundation
+import FirebaseFirestore
+
+fileprivate enum FireStoreCollections: String {
+    case users
+    case posts
+}
+
+enum SortingCriteria: String {
+    case fromNewestToOldest = "dateCreated"
+    var shouldSortDescending: Bool {
+        switch self {
+        case .fromNewestToOldest:
+            return true
+        }
+    }
+}
+
+class FirestoreService {
+    
+    static let manager = FirestoreService()
+    
+    private let db = Firestore.firestore()
+    
+    // MARK: - Creating and Updating Users
+    func createAppUser(user: AppUser, completion: @escaping (Result<(), Error>) -> ()) {
+        var fields: [String : Any] = user.fieldsDict
+        fields["dateCreated"] = Date()
+        db.collection(FireStoreCollections.users.rawValue).document(user.uid).setData(fields) { (error) in
+            if let error = error {
+                completion(.failure(error))
+            }
+            completion(.success(()))
+        }
+    }
+    
+    func updateCurrentUser(userName: String? = nil, photoURL: URL? = nil, completion: @escaping (Result <(), Error>) -> ()) {
+        guard let userID = FirebaseAuthService.manager.currentUser?.uid else {
+            return
+        }
+        
+        var updateFields = [String : Any]()
+        if let user = userName {
+            updateFields["userName"] = user
+        }
+        
+        if let photo = photoURL {
+            updateFields["photoURL"] = photo.absoluteString
+        }
+        
+       // MARK: - The PUT Request
+        db.collection(FireStoreCollections.users.rawValue).document(userID).updateData(updateFields) { (error) in
+            if let error = error {
+                completion(.failure(error))
+            } else {
+                completion(.success(()))
+            }
+        }
+    }
+    
+    // MARK: - Posts
+    func createPost(post: Post, completion: @escaping (Result <(), Error>) -> ()){
+        var fields = post.fieldsDict
+        fields["dateCreated"] = Date()
+        db.collection(FireStoreCollections.posts.rawValue).addDocument(data: fields) { (error) in
+            if let error = error {
+                completion(.failure(error))
+            } else {
+                completion(.success(()))
+            }
+        }
+    }
+    
+    func getAllPosts(sortingCriteria: SortingCriteria? = nil, completion: @escaping (Result <[Post], Error>) -> ()) {
+        let completionHandler: FIRQuerySnapshotBlock = {(snapshot, error) in
+            if let error = error {
+                completion(.failure(error))
+            } else {
+                let posts = snapshot?.documents.compactMap({ (snapshot) -> Post? in
+                    let postID = snapshot.documentID
+                    let post = Post(from: snapshot.data(), id: postID)
+                    return post
+                })
+                completion(.success(posts ?? []))
+            }
+        }
+        
+        let collection = db.collection(FireStoreCollections.posts.rawValue)
+        if let sortingCriteria = sortingCriteria {
+            let query = collection.order(by: sortingCriteria.rawValue, descending: sortingCriteria.shouldSortDescending)
+            query.getDocuments(completion: completionHandler)
+        } else {
+            collection.getDocuments(completion: completionHandler)
+        }
+    }
+    
+    func getPosts(forUserID: String, completion: @escaping (Result <[Post], Error>) -> ()) {
+        
+        
+        db.collection(FireStoreCollections.posts.rawValue).whereField("creatorID", isEqualTo: forUserID).getDocuments { (snapshot, error) in
+            if let error = error {
+                completion(.failure(error))
+            } else {
+                let posts = snapshot?.documents.compactMap({ (snapshot) -> Post? in
+                    let postID = snapshot.documentID
+                    let post = Post(from: snapshot.data(), id: postID)
+                    return post
+                })
+                completion(.success(posts ?? []))
+            }
+        }
+    }
+    
+    private init() {}
+}
