8a53ec4b79b0424c2ad1d9f0f2ed3c71fed12086
NormanLiana <liananorman@pursuit.org>
2019-11-27 19:37:25 -0500

    adds Firebase Storage Service file to Firebase folder

---
+++ Pursuitstagram-Project/Firebase/FirebaseStorageService.swift
@@ -0,0 +1,9 @@
+//
+//  FirebaseStorageService.swift
+//  Pursuitstagram-Project
+//
+//  Created by Liana Norman on 11/27/19.
+//  Copyright © 2019 Liana Norman. All rights reserved.
+//
+
+import Foundation
